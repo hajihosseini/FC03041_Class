c92e2be142226b0fe542a532f02231346cdfdd08
Malihe Hajihosseini <m.hajihosseini95@gmail.com>
2024-10-29 09:51:06 +0330

    add sum

---
+++ S05/Sum.py
@@ -0,0 +1,8 @@
+def Sum():
+    n = 9
+    result = 0
+    for i in range(1,n+1,2):
+        result = result + i
+    print(result)
+
+Sum()
---
+++ S05/main.py
@@ -0,0 +1,5 @@
+for i in range(-10,-5,1):
+    print(i)
+
+for w in range(8):
+    print(w)
