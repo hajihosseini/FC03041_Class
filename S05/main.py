a8e8a6db4865f7a6c43b2921c337d4764afac1f5
Malihe Hajihosseini <m.hajihosseini95@gmail.com>
2024-10-29 10:27:27 +0330

    final update

--- S05/main.py
+++ S05/main.py
@@ -1,5 +1,20 @@
-for i in range(-10,-5,1):
-    print(i)
+# for i in range(-10,-5,1):
+#     print(i)
 
-for w in range(8):
-    print(w)
+# for w in range(8):
+#     print(w)
+
+
+print("+---------+")
+print("+" + "---------" + "+")
+
+print("a"+ " " +"b")
+
+a= 3
+b = 4
+print(a+b)
+
+
+print("+" + 9*"-" + "+")
+
+print(5//2)
